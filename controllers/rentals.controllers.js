57743b64eaa0165fd687d748b0fc83cd84e13e55
hardkill551 <hardkill194@gmail.com>
2023-05-15 15:02:56 -0300

    fix/postRentalById

--- controllers/rentals.controllers.js
+++ controllers/rentals.controllers.js
@@ -59,18 +59,19 @@ export async function postRentalsById(req,res){
     const {id} = req.params
     const returnDate = dayjs().format("YYYY-MM-DD")
     try{
+        const rent = await db.query("SELECT * FROM rentals WHERE rentals.id = $1", [id])
+        if(rent.rowCount === 0) return res.sendStatus(404)
         const games = await db.query(`SELECT games."pricePerDay", rentals."rentDate", rentals."daysRented" FROM rentals JOIN games ON games.id = rentals."gameId" WHERE rentals.id = $1`, [id])
         const nowDate = Date.parse(dayjs())/86400000
         const rentDate = Date.parse(games.rows[0].rentDate)/86400000
         const rentDays =  nowDate-rentDate
-        const rent = await db.query("SELECT * FROM rentals WHERE rentals.id = $1", [id])
-        if(rent.rowCount === 0) return res.sendStatus(404)
+
         if(rent.rows[0].returnDate!==null) return res.sendStatus(400)
         let delay;
         if(games.rows[0].daysRented<rentDays) {
-            delay = games.rows[0].daysRented-rentDays
-            const delayFee = games.rows[0].pricePerDay*(delay)
-            await db.query(`UPDATE rentals SET "returnDate" = $1, "delayFee" = $2 WHERE rentals.id = $3`,[returnDate, Math.floor(delayFee),id])
+            delay = rentDays-games.rows[0].daysRented
+            const delayFee = games.rows[0].pricePerDay*(Math.floor(delay))
+            await db.query(`UPDATE rentals SET "returnDate" = $1, "delayFee" = $2 WHERE rentals.id = $3`,[returnDate, delayFee,id])
             return res.sendStatus(200)
         }
         
